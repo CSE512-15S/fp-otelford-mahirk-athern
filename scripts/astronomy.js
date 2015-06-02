ef6604db648d63d5274746c5fefa5ea64402a352
Mahir Kothary <mahirk@Mahirs-MBP.hsd1.wa.comcast.net>
2015-06-01 23:29:02 -0700

    added histograms

--- scripts/astronomy.js
+++ scripts/astronomy.js
@@ -29,74 +29,42 @@ var hist1 = d3.select("body")
 	.append("svg")
   .attr('class', 'chart')
 		.attr("width", width)
-		.attr("height", (height - margin.top - margin.bottom) + 10)
+		.attr("height", (height - 2*margin.top - 2*margin.bottom))
 	.append("g").attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
-    // Generate a Bates distribution of 10 random variables.
-    // var values = d3.range(100).map(d3.random.bates(10));
-    //
-    // // A formatter for counts.
-    // var formatCount = d3.format(",.0f");
-    // var x = d3.scale.linear()
-    //     .domain([0, 1])
-    //     .range([0, width/4 - 10]);
-    //
-    // // Generate a histogram using twenty uniformly-spaced bins.
-    // var data = d3.layout.histogram()
-    //     .bins(x.ticks(10))
-    //     (values);
-    //
-    // var y = d3.scale.linear()
-    //     .domain([0, d3.max(data, function(d) { return d.y; })])
-    //     .range([height/2 - margin.top - margin.bottom, 0]);
-    //
-    // var histx = d3.svg.axis()
-    //     .scale(x)
-    //     .orient("bottom");
-    //
-    // var histy = d3.svg.axis()
-    //         .scale(y)
-    //         .orient("left");
-    //
-    // var bar = hist1.selectAll(".bar")
-    //     .data(data)
-    //   .enter().append("g")
-    //     .attr("class", "bar")
-    //     .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
-    //
-    // bar.append("rect")
-    //     .attr("x", 1)
-    //     .attr("width", x(data[0].dx) - 1)
-    //     .attr("height", function(d) { return height/2 - margin.top - margin.bottom - y(d.y); });
-    //
-    // bar.append("text")
-    //     .attr("dy", ".75em")
-    //     .attr("y", 6)
-    //     .attr("x", x(data[0].dx) / 2)
-    //     .attr("text-anchor", "middle")
-    //     .text(function(d) { return formatCount(d.y); });
-    //
-    // hist1.append("g")
-    //     .attr("class", "x axis")
-    //     .attr("transform", "translate(0," + (height/2 - margin.top - margin.bottom) + ")")
-    //     .call(histx);
-    //
-    // hist1.append("g")
-    //     .attr("class", "y axis")
-    //     .call(histy)
-    //     .append("text")
-    //     .attr("transform", "rotate(-90)")
-    //     .attr("y", 6)
-    //     .attr("dy", ".7em")
-    //     .style("text-anchor", "end")
-    //     .text("Number of Galaxies");
 
+var hist2 = d3.select("body")
+  	.append("svg")
+    .attr('class', 'chart')
+  		.attr("width", width)
+  		.attr("height", (height - 2*margin.top - 2*margin.bottom) + 10)
+  	.append("g").attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
 
-d3.csv("trial.csv", function(error, data) {
-  var vals = [];
+var hist3 = d3.select("body")
+    	.append("svg")
+      .attr('class', 'chart')
+    		.attr("width", width)
+    		.attr("height", (height - 2*margin.top - 2*margin.bottom) + 10)
+    	.append("g").attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+var hist4 = d3.select("body")
+      	.append("svg")
+        .attr('class', 'chart')
+      		.attr("width", width)
+      		.attr("height", (height - 2*margin.top - 2*margin.bottom) + 10)
+      	.append("g").attr('transform', 'translate(' + margin.left + ', ' + margin.top + ')');
+
+d3.csv("data_clean.csv", function(error, data) {
+  var valshist1 = [];
+  var valshist2 = [];
+  var valshist3 = [];
+  var valshist4 = [];
   data.forEach(function(d) {
       d.SFR = +d.SFR;
       d.mass = +d.mass;
-      vals.push(+d["Z_Dopita"]);
+      valshist1.push(+d["Z_Dopita"]);
+      valshist2.push(+d["sSFR"]);
+      valshist3.push(+d["Z_Mannucci"]);
+      valshist4.push(+d["mass_err"]);
   });
 
 //SCATTER PLOT
@@ -136,6 +104,7 @@ d3.csv("trial.csv", function(error, data) {
         .attr("cy", yMap)
         .style("fill", "#333")
         .on("mouseover", function(d) {
+          d3.select(this).attr("r", 4 ).style("fill", "#79B441");
           tooltip.transition()
                .duration(200)
                .style("opacity", .9);
@@ -144,29 +113,27 @@ d3.csv("trial.csv", function(error, data) {
                .style("top", (d3.event.pageY - 28) + "px");
       })
       .on("mouseout", function(d) {
+        d3.select(this).attr("r", 3.5 ).style("fill", "#333");
           tooltip.transition()
                .duration(500)
                .style("opacity", 0);
       });
 
 // HISTOGRAMS
-//
+// HISTOGRAMS 1
 
-console.log(vals);
-
-var numOfBin = 3;
+var tickshist1 = 12;
 
 // A formatter for counts.
 var formatCount = d3.format(",.0f");
 var x = d3.scale.linear()
-    .domain([Math.floor(d3.min(vals)), Math.ceil(d3.max(vals))])
-    .range([0, width]);
+    .domain([Math.floor(d3.min(valshist1)), Math.ceil(d3.max(valshist1))])
+    .range([0, width/2]);
 
 // Generate a histogram using twenty uniformly-spaced bins.
 var bardata = d3.layout.histogram()
-    .bins(numOfBin)
-    (vals);
-
+    .bins(x.ticks(tickshist1))
+    (valshist1);
 
 
 var y = d3.scale.linear()
@@ -187,22 +154,34 @@ var bar = hist1.selectAll(".bar")
     .attr("class", "bar")
     .attr("transform", function(d) { return "translate(" + x(d.x) + "," + y(d.y) + ")"; });
 
+var exactwidth = (((width/2)/x.ticks(tickshist1).length) - 1)
+
 bar.append("rect")
     .attr("x", 1)
-    .attr("width", ((width/4)/numOfBin) - 1)
+    .attr("width", exactwidth)
     .attr("height", function(d) { return height/2 - margin.top - margin.bottom - y(d.y); });
 
 bar.append("text")
-    .attr("dy", ".75em")
+    .attr("dy", "-1em")
+    .attr("class", "histext")
     .attr("y", 6)
-    .attr("x", x(bardata[0].dx) / 2)
+    .attr("x", exactwidth/2 )
     .attr("text-anchor", "middle")
     .text(function(d) { return formatCount(d.y); });
 
 hist1.append("g")
     .attr("class", "x axis")
     .attr("transform", "translate(0," + (height/2 - margin.top - margin.bottom) + ")")
-    .call(histx);
+    .call(histx)
+    .append("text")
+      .attr("class", "label")
+      .attr("dx", width/2-margin.left)
+      .attr("dy", "3em")
+      .style("text-anchor", "end")
+      .text("Z")
+      .append("tspan")
+      .attr("baseline-shift", "sub")
+      .text("Dopita");
 
 hist1.append("g")
     .attr("class", "y axis")
@@ -213,6 +192,220 @@ hist1.append("g")
     .attr("dy", ".7em")
     .style("text-anchor", "end")
     .text("Number of Galaxies");
+
+//  HISTOGRAMS 2
+
+
+var tickshist2 = 10;
+
+var x2 = d3.scale.linear()
+    .domain([Math.floor(d3.min(valshist2))-1, Math.ceil(d3.max(valshist2))+1])
+    .range([0, width/2]);
+
+// Generate a histogram using twenty uniformly-spaced bins.
+var bardata2 = d3.layout.histogram()
+    .bins(x2.ticks(tickshist2))
+    (valshist2);
+
+var y2 = d3.scale.linear()
+    .domain([0, d3.max(bardata2, function(d) { return d.y; })])
+    .range([height/2 - margin.top - margin.bottom, 0]);
+
+var hist2x = d3.svg.axis()
+    .scale(x2)
+    .orient("bottom");
+
+var hist2y = d3.svg.axis()
+        .scale(y2)
+        .orient("left");
+
+var bar2 = hist2.selectAll(".bar")
+    .data(bardata2)
+  .enter().append("g")
+    .attr("class", "bar")
+    .attr("transform", function(d) { return "translate(" + x2(d.x) + "," + y2(d.y) + ")"; });
+
+
+var exactwidth2 = (((width/2)/x2.ticks(tickshist2).length) - 1)
+
+bar2.append("rect")
+    .attr("x", 1)
+    .attr("width", exactwidth2)
+    .attr("height", function(d) { return height/2 - margin.top - margin.bottom - y2(d.y); });
+
+bar2.append("text")
+    .attr("dy", "-1em")
+    .attr("class", "histext")
+    .attr("y", 6)
+    .attr("x", exactwidth2/2 )
+    .attr("text-anchor", "middle")
+    .text(function(d) { return formatCount(d.y); });
+
+hist2.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + (height/2 - margin.top - margin.bottom) + ")")
+    .call(hist2x)
+    .append("text")
+      .attr("class", "label")
+      .attr("dx", width/2-margin.left)
+      .attr("dy", "3em")
+      .style("text-anchor", "end")
+      .text("sSFR")
+      .append("tspan")
+      .attr("baseline-shift", "sub")
+      .text("");
+
+hist2.append("g")
+    .attr("class", "y axis")
+    .call(hist2y)
+    .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".7em")
+    .style("text-anchor", "end")
+    .text("Number of Galaxies");
+
+//  HISTOGRAMS 3
+
+
+var tickshist3 = 10;
+
+    var x3 = d3.scale.linear()
+        .domain([Math.floor(d3.min(valshist3))-1, Math.ceil(d3.max(valshist3))+1])
+        .range([0, width/2]);
+
+    // Generate a histogram using twenty uniformly-spaced bins.
+    var bardata3 = d3.layout.histogram()
+        .bins(x3.ticks(tickshist3))
+        (valshist3);
+
+    var y3 = d3.scale.linear()
+        .domain([0, d3.max(bardata3, function(d) { return d.y; })])
+        .range([height/2 - margin.top - margin.bottom, 0]);
+
+    var hist3x = d3.svg.axis()
+        .scale(x3)
+        .orient("bottom");
+
+    var hist3y = d3.svg.axis()
+            .scale(y3)
+            .orient("left");
+
+    var bar3 = hist3.selectAll(".bar")
+        .data(bardata3)
+      .enter().append("g")
+        .attr("class", "bar")
+        .attr("transform", function(d) { return "translate(" + x3(d.x) + "," + y3(d.y) + ")"; });
+
+
+    var exactwidth3 = (((width/2)/x3.ticks(tickshist3).length) - 1);
+    bar3.append("rect")
+        .attr("x", 1)
+        .attr("width", exactwidth3)
+        .attr("height", function(d) { return height/2 - margin.top - margin.bottom - y3(d.y); });
+
+    bar3.append("text")
+        .attr("dy", "-1em")
+        .attr("class", "histext")
+        .attr("y", 6)
+        .attr("x", exactwidth3/2 )
+        .attr("text-anchor", "middle")
+        .text(function(d) { return formatCount(d.y); });
+
+    hist3.append("g")
+        .attr("class", "x axis")
+        .attr("transform", "translate(0," + (height/2 - margin.top - margin.bottom) + ")")
+        .call(hist3x)
+        .append("text")
+          .attr("class", "label")
+          .attr("dx", width/2-margin.left)
+          .attr("dy", "3em")
+          .style("text-anchor", "end")
+          .text("Z")
+          .append("tspan")
+          .attr("baseline-shift", "sub")
+          .text("Mannucci");
+
+    hist3.append("g")
+        .attr("class", "y axis")
+        .call(hist3y)
+        .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".7em")
+        .style("text-anchor", "end")
+        .text("Number of Galaxies");
+
+// /  HISTOGRAMS 4
+
+
+var tickshist4 = 20;
+
+  var x4 = d3.scale.linear()
+      .domain([Math.floor(d3.min(valshist4))-1, Math.ceil(d3.max(valshist4))+1])
+      .range([0, width/2]);
+
+  // Generate a histogram using twenty uniformly-spaced bins.
+  var bardata4 = d3.layout.histogram()
+      .bins(x4.ticks(tickshist4))
+      (valshist4);
+
+  var y4 = d3.scale.linear()
+      .domain([0, d3.max(bardata4, function(d) { return d.y; })])
+      .range([height/2 - margin.top - margin.bottom, 0]);
+
+  var hist4x = d3.svg.axis()
+      .scale(x4)
+      .orient("bottom");
+
+  var hist4y = d3.svg.axis()
+          .scale(y4)
+          .orient("left");
+
+  var bar4 = hist4.selectAll(".bar")
+      .data(bardata4)
+    .enter().append("g")
+      .attr("class", "bar")
+      .attr("transform", function(d) { return "translate(" + x4(d.x) + "," + y4(d.y) + ")"; });
+
+
+  var exactwidth4 = (((width/2)/x4.ticks(tickshist4).length) - 1);
+  bar4.append("rect")
+      .attr("x", 1)
+      .attr("width", exactwidth4)
+      .attr("height", function(d) { return height/2 - margin.top - margin.bottom - y4(d.y); });
+
+  bar4.append("text")
+      .attr("dy", "-1em")
+      .attr("class", "histext")
+      .attr("y", 6)
+      .attr("x", exactwidth4/2 )
+      .attr("text-anchor", "middle")
+      .text(function(d) { return formatCount(d.y); });
+
+  hist4.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + (height/2 - margin.top - margin.bottom) + ")")
+      .call(hist4x)
+      .append("text")
+        .attr("class", "label")
+        .attr("dx", width/2-margin.left)
+        .attr("dy", "3em")
+        .style("text-anchor", "end")
+        .text("mass")
+        .append("tspan")
+        .attr("baseline-shift", "sub")
+        .text("err");
+
+  hist4.append("g")
+      .attr("class", "y axis")
+      .call(hist4y)
+      .append("text")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", ".7em")
+      .style("text-anchor", "end")
+      .text("Number of Galaxies");
 
 });
 
